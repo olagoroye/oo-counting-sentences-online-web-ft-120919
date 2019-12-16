e5a660d20a7013b1d8db3a5379f1f4bef25dbd28
olusola olagoroye <solaolagoroye@gmail.com>
2019-12-16 19:32:16 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
+    array = []
+    array = self.split
+    counter = 0 
+    array.each {|x| 
+    if x.sentence? || x.question? || x.exclamation?
+    counter += 1 
+    end
+    }
+# array.each do |x|
+#   if x.sentence? || x.question? || x.exclamation?
+#     counter += 1 
+  
+  counter
 
-  end
 end
+
+end
+
